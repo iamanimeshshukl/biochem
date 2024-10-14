54c2b9fdea1012677b52f3e56a4728f8a8055194
iamanimeshshukl <animeshshuklafoundation@gmail.com>
2024-10-14 20:57:11 +0530

    new

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Components/Header'
+import Navbar from './Components/Navbar'
+import Footer from './Components/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Navbar />
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
