d0ecc11a5806c42bf10b349cde6065f31ad113f1
iamanimeshshukl <animeshshuklafoundation@gmail.com>
2024-10-14 20:49:00 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Header from './Components/Header'
 import Navbar from './Components/Navbar'
 import Footer from './Components/Footer'
 import Team from './Components/Team'
+import About from './Components/About'
 const App = () => {
   return (
     <div>
       <Header />
       <Navbar />
+      <About />
       <Team />
       <Footer/>
     </div>
