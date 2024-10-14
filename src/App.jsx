7225eadfbea56505aec25cc07cf2dab8b7023c52
iamanimeshshukl <animeshshuklafoundation@gmail.com>
2024-10-14 20:43:02 +0530

    jai shree ram

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import Header from './Components/Header'
 import Navbar from './Components/Navbar'
 import Footer from './Components/Footer'
-
+import Team from './Components/Team'
 const App = () => {
   return (
     <div>
       <Header />
       <Navbar />
+      <Team />
       <Footer/>
     </div>
   )
