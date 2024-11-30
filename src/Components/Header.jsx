dddc13ef2dbf0dd8be5b8036286b97d44dc2a0ae
Amay Mishra <amaymishra02@gmail.com>
2024-12-01 00:57:57 +0530

    imported

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,19 @@
 import React from 'react';
-
+import headerbg from "src/assets/headerbg.png"
+import mobileheader from "src/assets/mobileheader.png"
 const Header = () => {
   return (
     <div className='w-full'>
       {/* Desktop Image */}
       <img 
         className='hidden md:block h-[40%] w-full object-contain' 
-        src="src/assets/headerbg.png" 
+        src={headerbg}
         alt="Header Background for Desktop" 
       />
       {/* Mobile Image */}
       <img 
         className='block md:hidden h-auto w-full object-contain' 
-        src="src/assets/mobileheader.png" 
+        src={mobileheader}
         alt="Header Background for Mobile" 
       />
     </div>
