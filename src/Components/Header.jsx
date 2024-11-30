c15e9ab5965f2e6dc7d9c34c4b2946a6c954eb6c
iamanimeshshukl <animeshshuklafoundation@gmail.com>
2024-11-30 23:51:52 +0530

    new

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { motion } from 'framer-motion'; // Import Framer Motion for animations
+import logo2 from "../assets/BIO-METACON_Logo-removebg-preview.png";
+import hbtu from '../assets/hbtu logo.png';
+import { div } from 'framer-motion/client';
 
 const Header = () => {
   return (
    <div className=''>
-         <img className='h-[45vh] lg:h-[55vh] w-[100%]' src="src\assets\headerbg.png" alt="" />
+         <img className='h-[36vh] lg:h-[55vh] w-full' src="src\assets\headerbg.png" alt="" />
    </div>
   );git
 };
