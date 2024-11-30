1954fee88da4e7f6500d5f92857fd21f7f0ef0e5
Swapnil <swapnildev2003@gmail.com>
2024-11-30 22:41:01 +0530

    done with homepage

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,9 +7,9 @@ import { div } from 'framer-motion/client';
 const Header = () => {
   return (
    <div className=''>
-         <img className='h-[55vh] w-full' src="src\assets\headerbg.png" alt="" />
+         <img className='h-[36vh] lg:h-[55vh] w-full' src="src\assets\headerbg.png" alt="" />
    </div>
-  );
+  );git
 };
 
 export default Header;
